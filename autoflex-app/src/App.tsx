751593a531c3e66666a414ea9531567bf9a4c8b6
fernandosenacruz <fernandosenacruz@gmail.com>
2022-05-24 18:53:38 -0300

    Update App.js, App.test.js, and 2 more files...

---
+++ autoflex-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Product from './component/Product';
+import Feedstock from './component/Feedstock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Product />
+      <Feedstock />
+    </div>
+  );
+}
+
+export default App;
